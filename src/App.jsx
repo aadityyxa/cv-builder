4aaf21ec7cec81b12725ecafa4a59ccd13a372b5
aadityaa <aditya.oza256@gmail.com>
2023-08-28 23:13:07 +0530

    added state management functionality for education form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 // import { useState } from 'react'
 import './App.css'
 import {PersonalDetails, Education} from './components/personal'
-
-
-
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid'; 
 
 function App() {
-  <>
-    <PersonalDetails />
-    <Education />
-  </>
+
+  const [edArray, setEdArray] = useState([]);
+  const array = [...edArray]; 
+
+    function saveEducationDetails() {
+    const newEducation = {
+      id:uuidv4(),
+      name:document.querySelector('#school').value,
+      degree:document.querySelector('#degree').value,
+      yog:document.querySelector('#yog').value,
+      location:document.querySelector('#location').value
+
+    }
+
+    array.push(newEducation); 
+    setEdArray(array); 
+
+  }
+
+  return (
+    <>
+      <PersonalDetails handleChange={saveEducationDetails}/>
+      <Education />
+    </>
+  )
 }
 
 export default App
