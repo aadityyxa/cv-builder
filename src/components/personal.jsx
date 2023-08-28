66c0ec01d68ed51375cb82167876ea364d1906aa
aadityaa <aditya.oza256@gmail.com>
2023-08-28 22:54:24 +0530

    added personal details form and styles

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -9,3 +9,21 @@ function FormItem({typeAttr, nameAttr, idAttr, text, change}) {
     )
 }
 
+function PersonalDetails({handleChange}) {
+
+    return (
+        <div className="details">
+            <form>
+                <h3 className="heading">Personal Details</h3>
+                <FormItem typeAttr="text" nameAttr='name' idAttr='name' text='Name' change={handleChange} />
+                <FormItem typeAttr='email' nameAttr='email' idAttr='email' text='Email' change={handleChange} />
+                <FormItem typeAttr='tel' nameAttr='phone' idAttr='phone' text='Phone' change={handleChange}/>
+                <FormItem typeAttr='text' nameAttr='address' idAttr='address' text='Address' change={handleChange}/>
+            </form>
+        </div>
+    ) 
+}
+
+export {PersonalDetails}; 
+
+
