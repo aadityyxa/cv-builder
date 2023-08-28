12d9574b159c222853f74d2d24577e8b6a0c1d10
aadityaa <aditya.oza256@gmail.com>
2023-08-28 23:06:46 +0530

    added education details form, styles and specifications

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -1,4 +1,9 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+import Icon from '@mdi/react';
+import { mdiTrashCanOutline } from '@mdi/js';
+
+
 function FormItem({typeAttr, nameAttr, idAttr, text, change}) {
 
     return(
@@ -37,6 +42,52 @@ function List({list, klasse, onAdd, heading}) {
 
 }
 
-export {PersonalDetails}; 
+function Education({array, onChange, onDelete}) {
+    const [formOpen, setFormOpen] = useState(true);
+    
+    function toggleForm() {
+        if(formOpen === true) {
+            setFormOpen(false);
+        }else{
+            setFormOpen(true);     
+        }
+    }
+
+    const listItems = array.map((education) => {
+    return (    
+        <div className="form-list-item" key={education.id}>
+            <h4>{education.name}, Class Of {education.yog}</h4>
+            <div className="icon" onClick={() => {
+                onDelete(education.id);
+            }}>
+                <Icon path={mdiTrashCanOutline} size={1} />
+            </div>
+        </div>
+    )}); 
+
+    return(
+        <div className="details">
+            {formOpen? 
+            <List list={listItems} klasse='education' onAdd={toggleForm} heading='Education' />
+            :<form>
+                <FormItem typeAttr='text' nameAttr='school' idAttr='school' text='School' />
+                <FormItem typeAttr='text' nameAttr='degree' idAttr='degree' text='Degree' />
+                <FormItem typeAttr='text' nameAttr='yog' idAttr='yog' text='Year Of Graduation' />
+                <FormItem typeAttr='text' nameAttr='location' idAttr='location' text='Location' />
+                <button type="submit" onClick={(e) => {
+                    onChange();
+                    e.preventDefault();
+                    toggleForm(); 
+                    
+                }}>Save</button>
+            </form>}
+        </div>
+
+    )
+
+}
+
+
+export {PersonalDetails, Education}; 
 
 
